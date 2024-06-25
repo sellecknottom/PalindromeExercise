0b9333c34092c25a9c1ec23c16342546a3069550
Selleck Elliott <selleck.elliott@gmail.com>
2024-06-24 21:20:55 -0600

    exercise complete

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public static bool IsPalindrome(string word)
+        {
+            char[] chars = word.ToCharArray();
+
+            Array.Reverse(chars);
+
+            string reversWord = new string(chars);
+
+            if (word == reversWord)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,20 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("tattarrattat", true)]
+        [InlineData("rudolph", false)]
+        [InlineData("madam", true)]
+        [InlineData("malayalam", true)]
+        [InlineData("ball", false)]
+        public void PalindromeTester(string word, bool expected)
         {
-
+            //Arrange
+            //var wordSmith = new WordSmith();
+            //Act
+            bool actual = WordSmith.IsPalindrome(word);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
